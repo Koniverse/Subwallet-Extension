89e2c57ee7395048abff0c87e5f8528b465561f4
Frenkie Nguyen <nguyentiendung.me@gmail.com>
2025-02-21 10:23:23 +0700

    [Issue-3864] feat: Add constant for unified address in Polkadot system

--- packages/extension-koni-ui/src/constants/chain.ts
+++ packages/extension-koni-ui/src/constants/chain.ts
@@ -2,3 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const ASSET_HUB_CHAIN_SLUGS = ['paseo_assethub', 'westend_assethub', 'rococo_assethub', 'statemine', 'statemint'];
+
+// TODO: This is for dev purposes. Will update to edit the list of chains online via API.
+export const UNIFIED_POLKADOT_CHAIN_SLUGS = ['bifrost_dot', 'statemint', 'astar', 'acala', 'phala', 'integritee', 'robonomics'];
+export const UNIFIED_CHAIN_SS58_PREFIX = 0;
